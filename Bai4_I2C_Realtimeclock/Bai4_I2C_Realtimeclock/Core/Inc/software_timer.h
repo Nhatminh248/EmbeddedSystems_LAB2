3019ea5983878da8b661c4d098f3a446776697ae
minhnhat248-admin <nhatminhle248@gmail.com>
2024-11-18 22:46:53 +0700

    Add Source Code of Chapter 4

---
+++ Bai4_I2C_Realtimeclock/Bai4_I2C_Realtimeclock/Core/Inc/software_timer.h
@@ -0,0 +1,25 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 24, 2023
+ *      Author: HaHuyen
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "tim.h"
+#include "led_7seg.h"
+#include "type.h"
+
+#define FREQUENCY_OF_TIM	1000    // Hz
+#define INCREASE_TIME		200
+#define BLINKING_TIME		1000 // blink in 2Hz
+#define NUMBER_OF_TIMER 	3
+
+
+void set_timer(unsigned i, unsigned int time);
+void run_timer(void);
+bool flag_timer(unsigned i);
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
